224819dbca488d418d056171c05f60fdcbcdde40
ruyue-zhang <772346730@qq.com>
2020-12-16 15:44:05 +0800

    [ruyue] feat:implement login layout

--- app/src/main/java/com/ruyue/note/login/LoginActivity.java
+++ app/src/main/java/com/ruyue/note/login/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.ruyue.note;
+package com.ruyue.note.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ruyue.note.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
